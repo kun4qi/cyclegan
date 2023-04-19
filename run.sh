76cf7e5feab7265081ed63711b663966699a0a8f
kun4qi <101441711+kun4qi@users.noreply.github.com>
2023-04-19 21:27:02 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,6 +6,6 @@
 #$-cwd
 
 source /etc/profile.d/modules.sh
-module load gcc/11.2.0 python/3.7/3.7.13 cuda/11.6 cudnn/8.4
+module load gcc/12.2.0 python/3.10/3.10.10 cuda/11.6 cudnn/8.4
 
 ~/brats/bin/python3 train_cyclegan.py -c config_train_cyclegan.json -s
